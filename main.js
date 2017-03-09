54c7af35d0ad76dd331ecb17daef79725351cdc9
Terrance Corley <terrancecorley@outlook.com>
2017-03-09 11:15:41 -0800

    Initial push of site.

---
+++ main.js
@@ -0,0 +1,22 @@
+const div = document.querySelector('div');
+const button = document.querySelector('button');
+const msg = document.querySelectorAll('.msg');
+
+div.addEventListener('mouseover', (event) => {
+  if ( event.target.className === 'msg' ) {
+    event.target.style.textTransform = 'uppercase';
+    event.target.style.color = 'blue';
+    event.target.style.fontWeight = 'bold';
+    event.target.style.fontSize = '32px';
+  }
+});
+
+button.addEventListener('click', () => {
+  button.textContent = 'You lazy bum. :)';
+  for ( let i = 0; i < msg.length; i +=1 ) {
+    msg[i].style.color = 'blue';
+    msg[i].style.textTransform = 'uppercase';
+    msg[i].style.fontWeight = 'bold';
+    msg[i].style.fontSize = '32px';
+  }
+});
